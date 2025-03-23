d94c1800f3cf05cb8f6a6ad52a3d10bedeff2891
Keys <keys14052002@gmail.com>
2025-03-23 14:01:31 +0000

    Change the description

--- partials/_header.php
+++ partials/_header.php
@@ -7,7 +7,7 @@
         <html land='en-US'>
             <head>
                 <meta charset='UTF-8'>
-                <meta name='description' content='Dynamic image gallery'>
+                <meta name='description' content='Use this site to organize a poll'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0 shrink-to-fit=no'>
                 <title>{$page->getTitle()}</title>
                 {$page->getStylesheet()}
